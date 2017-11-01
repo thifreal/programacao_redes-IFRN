23a4edd8c71fe5fcfd00ab477c8025f396f9473d
Thiago Freitas de Almeida <thiagofrdal@gmail.com>
2017-11-01 17:33:24 -0300

    Editando codigo 14_cliente

--- aula/14_socket_client_tcp.py
+++ aula/14_socket_client_tcp.py
@@ -6,7 +6,7 @@ PORT = 2000
 tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_client_socket.connect((HOST,PORT))
 message = "Hello World!"
-byte_msg = message.enconde('utf-8')
+byte_msg = message.encode('utf-8')
 tcp_client_socket.send(byte_msg)
 data = tcp_client_socket.recv(1024)
 tcp_client_socket.close
