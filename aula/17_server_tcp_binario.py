92a19ef4f53752f996f35f8b3f41b207f9de4f63
Thiago Freitas de Almeida <thiagofrdal@gmail.com>
2017-12-04 17:22:58 -0300

    ajeitando servidor binario

--- aula/17_server_tcp_binario.py
+++ aula/17_server_tcp_binario.py
@@ -1,18 +1,19 @@
 import socket
 from threading import Thread
-from SocketServer import ThreadingMixIn
+from socketserver import ThreadingMixIn
 
 TCP_IP = 'localhost'
 TCP_PORT = 9001
 BUFFER_SIZE = 1024
 
-class ClientThread(Thread)
+class ClientThread(Thread):
+
     def __init__(self,ip,port,sock):
         Thread.__init__(self)
         self.ip = ip
         self.port = port
         self.sock = sock
-        print("Nova thread inicializada "+ip+":"+str(port))
+        print("Nova thread inicializada para "+ip+":"+str(port))
 
     def run(self):
         filename='mytext.txt'
@@ -27,19 +28,20 @@ class ClientThread(Thread)
                 self.sock.close()
                 break
 
+
 tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-tcpsock.bind(TCP_IP TCP_PORT)
+tcpsock.bind((TCP_IP, TCP_PORT))
 threads = []
 
 while True:
     tcpsock.listen(5)
     print("Aguardando conexões...")
-    (conn(ip,port)) = tcpsock.accept()
+    (conn,(ip,port)) = tcpsock.accept()
     print("Conexão recebida de:"+ip+":"+str(port))
     newthread = ClientThread(ip,port,conn)
     newthread.start()
     threads.append(newthread)
 
 for t in threads:
-    t.join()  
+    t.join()
